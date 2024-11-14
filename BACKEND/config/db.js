cfcefa606bdb11ed83fdb3f5d83a5605e94ba8e9
Terkihacks <raymondmunguti163@gmail.com>
2024-11-15 00:12:46 +0300

    Tested the database connection

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -8,8 +8,16 @@ const pool = mysql.createPool({
     user:process.env.DB_USER,
     database:process.env.DB_NAME,
     password:process.env.DB_PASSWORD
- })
-
+ });
+ // Test the connection to the database
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Database connection failed:', err.message);
+    } else {
+        console.log('Database connected successfully!');
+        connection.release(); // Release the connection back to the pool
+    }
+});
 // Lets export the db.js to be able to be available in all our files
 module.exports = pool.promise()
  //The promise will help us to use the await and async function when creating the web server
